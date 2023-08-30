971d3b2af6057ea85c180da88989f5e01355ea20
Olesya M <olesayvalerevna@mail.ru>
2023-08-30 17:33:18 +0400

    навигация

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -17,17 +17,23 @@ export default function Navigation({ loggedIn }) {
     <nav className="navigation">
       {!loggedIn ? (
         <div className="navigation__auth">
-          <Link className="navigation__registr" to="/signup">Регистрация</Link>
-          <Link className="navigation__login" to="/signin">Войти</Link>
+          <NavLink className="navigation__registr" to="/signup">Регистрация</NavLink>
+          <NavLink className="navigation__login" to="/signin">Войти</NavLink>
         </div>
       ) : (
         <>
-          <div className="navigation__movies navigation__movies_type_desktop">
-            <Link className="navigation__movies-link" to="/movies">Фильмы</Link>
-            <Link className="navigation__saved-movies-link" to="/saved-movies">Сохранённые фильмы</Link>
-          </div>
-          <div className="navigation__profile navigation-desktop">
-            <Link to="/profile" className="navigation__profile-link navigation__profile-link_type_desktop"><span className="navigation__profile-icon"></span></Link>
+          <nav className="navigation__movies navigation__movies_type_desktop">
+            <NavLink  className={({ isActive}) => isActive ? "navigation__movies-link_active" : "navigation__movies-link"
+  } to="/movies">Фильмы</NavLink>
+
+            <NavLink className={({ isActive}) =>
+    isActive ? "navigation__saved-movies-link_active" : "navigation__saved-movies-link"
+  } to="/saved-movies">Сохранённые фильмы</NavLink>
+          </nav>
+
+
+          <div className="navigation__profile navigation__profile_type_desktop">
+            <Link to="/profile" className="navigation__profile-link"><span className="navigation__profile-icon"></span></Link>
           </div>
           <button className="navigation__menu-icon" onClick={handleMenuOpen} type="button"></button>
         </>
@@ -36,9 +42,12 @@ export default function Navigation({ loggedIn }) {
       <nav className={`navigation__mobile-menu ${isMenuOpen ? "navigation__mobile-menu_opened" : ""}`}>
         <div className="navigation__mobile-container">
           <nav className="navigation__mobile-link">
-            <NavLink to="/" className="menu_link" onClick={handleMenuClose}>Главная</NavLink>
-            <NavLink to="/movies" className="menu_link" onClick={handleMenuClose}>Фильмы</NavLink>
-            <NavLink to="/saved-movies" className="menu_link" onClick={handleMenuClose}>Сохранённые фильмы</NavLink>
+            <NavLink to="/" className={({ isActive}) => isActive ? "navigation__menu-link_active" : "navigation__menu-link"
+  } onClick={handleMenuClose}>Главная</NavLink>
+            <NavLink to="/movies" className={({ isActive}) => isActive ? "navigation__menu-link_active" : "navigation__menu-link"
+  } onClick={handleMenuClose}>Фильмы</NavLink>
+            <NavLink to="/saved-movies" className={({ isActive}) => isActive ? "navigation__menu-link_active" : "navigation__menu-link"
+  } onClick={handleMenuClose}>Сохранённые фильмы</NavLink>
           </nav>
           <Link to="/profile" className="navigation__profile-link"><span className="navigation__profile-icon" onClick={handleMenuClose}></span></Link>
         </div>
